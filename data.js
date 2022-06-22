18558ed88f019c83544027dee39e1178fb363ae3
Danny Cairns <dpcairns@gmail.com>
2022-06-22 13:10:13 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,27 @@
+const doggies = {
+    Jeep: {
+       age: 2,
+       food: {
+           type: 'kibble',
+           price: 4
+       },
+      nicknames: ['the lizard king', 'professor jeeperson']
+    },
+    Hazel: {
+        age: 4,
+        food: {
+            type: 'canned',
+            price: 5
+        },
+        nicknames: ['good girl!', 'precious thing!']
+
+    },
+    Spot: {
+       age: 7, 
+       food: {
+           type: 'kibble',
+           price: 5
+       },
+       nicknames: ['good boy!', 'mr special!', 'my little monster']
+    }
+}
